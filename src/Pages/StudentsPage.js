47d9757c0a0f63085bf6efda0c1584f81a14ee33
JESKA JENIPHER <jeska.jenipher@student.moringaschool.com>
2022-09-01 15:55:47 +0300

    add a custom hook

--- src/Pages/StudentsPage.js
+++ src/Pages/StudentsPage.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import { useEffect,useState,React } from 'react';
 //We need all students to be displayed here
 //We need to edit students details, add, or delete
 
 const StudentsPage = () => {
+  const [data, setData] = useState([]);
 
-  
+  //custom hook
+  function useFectchStudents() {
+    
+       useEffect(() => {
+         fetch("http://localhost:3000/students")
+           .then((res) => res.json())
+           .then((data) => setData(data));
+       }, []);
+  }
+
   return (
     <div>StudentsPage</div>
   )
