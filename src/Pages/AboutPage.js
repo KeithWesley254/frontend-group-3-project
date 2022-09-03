35ee7ec76f4d77cad71e75dbebced3ef1b35df26
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-09-03 18:14:09 +0300

    initial commit

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,11 +1,31 @@
-import React from 'react'
-//Fetch details about our school
-//Simply display Group members' names as the Board members for the school
-
 const AboutPage = () => {
-  return (
-    <div>AboutPage</div>
-  )
-}
+  return (  
+    <div className="about">
+      <h1 className="test">
+        About Us
+      </h1>
+      <p className="test1">
+      This is a School Management Software that helps to increase teachers’ engagement with students.
+       It offers understandable analytics and graphics that enable you to make data-driven decisions. 
+       This is one of the best School Management Software that simplifies admissions, enrollment, registrations, and many other processes.
 
-export default AboutPage
+      <h3><div className="test2">Features:</div></h3>
+
+        <ul>
+            <li>Personalized learning schedules</li>
+            <li>Provides Google Classroom Integration.</li>
+            <li>This is one of the best school ERP software that is customizable according to schools.</li>
+            <li>Enables teachers and students to easily communicate with each other.</li>
+            <li>Offers custom reporting facility.</li>
+            <li>Provides easy migration of school data.</li>
+             <li>This app can make the school administration process easy.</li>
+             <li> Demo: Yes</li>
+             <li> Mobile App: Yes</li>
+            
+        </ul>
+      </p>
+    </div>
+  );
+}
+ 
+export default AboutPage;
