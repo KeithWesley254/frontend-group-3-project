322dd0f211fec7a9a0ee6f5f51b1bc5ba6b63c78
+ <keithwesley187@gmail.com>
2022-09-04 16:24:50 +0300

    milestone: TeacherDetails component added

---
+++ src/Components/TeachersDetails.js
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function TeachersDetails({allTeachers}){
+
+    const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+    },
+    }));
+
+    const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+        border: 0,
+    },
+    }));
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Name</StyledTableCell>
+            <StyledTableCell align="right">specialisation</StyledTableCell>
+            <StyledTableCell align="right">Street_address</StyledTableCell>
+            <StyledTableCell align="right">Salary</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {allTeachers.map((teacher) => (
+            <StyledTableRow key={teacher.id}>
+              <StyledTableCell component="th" scope="row">
+                {teacher.name}
+              </StyledTableCell>
+              <StyledTableCell align="right">{teacher.specialisation}</StyledTableCell>
+              <StyledTableCell align="right">{teacher.street_address}</StyledTableCell>
+              <StyledTableCell align="right">{teacher.salary}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+
+
+export default TeachersDetails
--- src/Pages/TeachersPage.js
+++ src/Pages/TeachersPage.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import TeachersDetails from '../Components/TeachersDetails';
 //We need all teachers to be displayed here
 //We need to edit teachers details, add, or delete
 
 const TeachersPage = () => {
+  const [allTeachers, setAllTeachers] = useState([])
+
+  useEffect(() => {
+    fetch('https://group-3-backend-app.herokuapp.com/teachers')
+    .then(r => r.json())
+    .then(data => setAllTeachers(data))
+  }, []);
+
+  // console.log(allTeachers)
+
   return (
-    <div>TeachersPage</div>
+    <div>
+      <TeachersDetails allTeachers = {allTeachers}/>
+    </div>
   )
 }
 
